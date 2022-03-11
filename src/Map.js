ffa05d592996191ac17c30512a3a4a5d96189192
Dylan Nirvana <dylan@dylannirvana.com>
2022-03-11 10:28:03 -0500

    grabs text and image.name from image_sets.image_with_text

--- src/Map.js
+++ src/Map.js
@@ -4,12 +4,15 @@ import Data from "./data";
 export class Map extends Component {
     
     render() {
+
+      const data = Data.image_sets[0].image_with_text;
+
         return (
           <>
             <div>
               <h3>Using local JSON</h3>
               <p>Starting with image_with_text node, we are able to capture the text child and image.name child. That is what currently fits the API of the framework.</p>
-              {Data.image_with_text.map((item, i) => (
+              {data.map((item, i) => (
                 <ul key={i}>
                   <li>{item.text} </li>
                   <li>{item.image.name} </li>
